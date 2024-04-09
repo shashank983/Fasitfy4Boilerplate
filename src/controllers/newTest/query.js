9f1cf6a95d4299e97bac4083f87560859032aa9c
Shashank <shashank@cloudmpower.com>
2024-04-09 14:43:03 +0530

    fastify

---
+++ src/controllers/newTest/query.js
@@ -0,0 +1,12 @@
+import { createResponseObj } from "../../responseHandler/responseHandler.js";
+
+export const firstApi = async (req, reply) => {
+  let responseObject = { foo: "bar" };
+
+  createResponseObj(
+    responseObject,
+    { successMessage: "Api success", errorMessage: "Api failure" },
+    req.headers.transactionId,
+    reply
+  );
+};
